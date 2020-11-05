d5ac6eb39e7e1b76974d0d26773cf5a462ebd36c
zhnzhzz <zhnzhzz@gmail.com>
2020-11-05 10:54:07 +0800

    fix node drag issue

--- src/components/graph/Graph.jsx
+++ src/components/graph/Graph.jsx
@@ -237,6 +237,26 @@ export default class Graph extends React.Component {
             draggedNode.x += d3Event.dx;
             draggedNode.y += d3Event.dy;
 
+            // limit the node inside canvas boundaries
+            let { width, height } = this.state.config;
+            const adjustSize = draggedNode.size ? draggedNode.size / 2 : 0;
+            const left = adjustSize;
+            const right = width - adjustSize;
+            const top = adjustSize;
+            const bottom = height - adjustSize;
+
+            if (draggedNode.x < left) {
+                draggedNode.x = left;
+            } else if (draggedNode.x > right) {
+                draggedNode.x = right;
+            }
+
+            if (draggedNode.y < top) {
+                draggedNode.y = top;
+            } else if (draggedNode.y > bottom) {
+                draggedNode.y = bottom;
+            }
+
             // set nodes fixing coords fx and fy
             draggedNode["fx"] = draggedNode.x;
             draggedNode["fy"] = draggedNode.y;
